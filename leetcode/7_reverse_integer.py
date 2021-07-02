2993c90e2dafd3960222aff24b68a4a29ee6b3e2
Abu Shumon <abu.shumon@qvik.fi>
2021-07-03 00:55:46 +0100

    reverse integer python implementation

---
+++ leetcode/7_reverse_integer.py
@@ -0,0 +1,46 @@
+"""Reverse integer module:
+Given an integer, function returns reversed version
+of it if it's in the signed integer range;
+0 otherwise
+"""
+
+import math
+
+
+class Solution:
+    def reverse(self, x: int) -> int:
+
+        # checks if given integer is a 32-bit
+        # signed integer.
+        if not self.is_in_range(x):
+            return 0
+
+        # integer to string, reverse the
+        # string and then converts it back
+        # to integer
+        number_str = str(abs(x))
+        reversed_number = int(number_str[::-1])
+
+        # makes reversed integer a negative integer
+        # if given integer (i.e. x)is negative
+        if x < 0:
+            reversed_number = -reversed_number
+
+        return 0 if not self.is_in_range(reversed_number) else reversed_number
+
+    def is_in_range(self, x: int) -> bool:
+
+        if x < int(math.pow(-2, 31)) or x > math.pow(2, 31) - 1:
+            return False
+        else:
+            return True
+
+
+def main():
+    number = Solution()
+
+    print(number.reverse(1534236469))
+
+
+if __name__ == "__main__":
+    main()
