127c104fa6f9cdd4a2bad0995dce48968695867e
Abu Shumon <johnshumon@gmail.com>
2021-10-05 17:03:55 +0100

    added shortest path and dfs_traversal implementation

--- data-structures/directed_graph.py
+++ data-structures/directed_graph.py
@@ -10,6 +10,7 @@ Directed graph module
 # Graph with cycles
 
 from collections import defaultdict
+from typing import Any, Dict, List, Set
 
 
 class DGraph:
@@ -22,7 +23,7 @@ class DGraph:
     def __init__(self):
         self.adj_list = defaultdict(set)
 
-    def connect_edges(self, *edges) -> dict:
+    def connect_edges(self, *edges) -> Dict:
         """
         Builds graph for the given edges.
 
@@ -38,18 +39,80 @@ class DGraph:
 
         return self.adj_list
 
-#     def get_paths():
-#         pass
+    def get_paths(self, start: Any, end: Any, visited: List) -> List:
+        """
+        Returns all paths between two nodes.
 
-#     def get_shortest_path():
-#         pass
+        Accepts a pair (u, v) and finds all the possible
+        paths between u -> v if any.
+        """
+
+        # 'start' not in the dictionary key means
+        # there's no edge coming out of that node.
+        if start not in self.adj_list:
+            return []
+
+        visited = visited + [start]
+
+        # print(f"visited: {visited}")
+
+        if start == end:
+            return [visited]
+
+        paths = []
+
+        # For all the adjacent nodes of 'start'
+        # recur this function.
+        for node in self.adj_list[start]:
+            if node not in visited:
+                sub_paths = self.get_paths(node, end, visited)
+
+                for item in sub_paths:
+                    paths.append(item)
+
+        return paths
+
+    def get_shortest_path(self, start: Any, end: Any) -> List:
+        """
+        Returns shortest path between two nodes.
+
+        Accepts a pair (u, v), finds all the possible
+        paths between u -> v from 'get_path' method if
+        any. And then returns the shortest one.
+        """
+
+        # 'key' argument finds the min/max of an iterable based
+        # on what is defined as a key. Here key is length. Hence,
+        # of all the lists, min length list is returned.
+        return min(self.get_paths(start, end, []), key=len)
 
 #     def is_disjoint():
 #         pass
 
 
-# def dfs_traversal():
-#     pass
+def dfs_traversal(graph: DGraph, node: Any, dfs_order: List[Any], visited: Set[Any]) -> Set:
+    """
+    Does depth first search (DFS) over a graph.
+
+    Begins with the starting node and returns the
+    DFS traversal order of the nodes in the graph.
+    """
+
+    if node is None or graph is None:
+        return visited
+
+    if node not in visited:
+        # Set unions. list is used to keep the traversal
+        # order correct. Because both 'union & set.add'
+        # different order in different times.
+        visited |= set(node)
+        dfs_order.append(node)
+
+        for neighbour in graph.adj_list[node]:
+            dfs_traversal(graph, neighbour, dfs_order, visited)
+
+    return dfs_order
+
 
 # def bfs_walk():
 #     pass
@@ -58,8 +121,15 @@ class DGraph:
 if __name__ == "__main__":
 
     dg = DGraph()
-    dg.connect_edges(("a", "b"), ("a", "d"), ("b", "a"), ("b", "c"))
-    dg.connect_edges(("c", "b"), ("c", "d"), ("d", "a"), ("d", "c"))
-    dg.connect_edges(("e", "f"), ("e", "a"))
+    # dg.connect_edges(("a", "b"), ("a", "d"), ("b", "a"), ("b", "c"))
+    # dg.connect_edges(("c", "b"), ("c", "d"), ("d", "a"), ("d", "c"))
+    # dg.connect_edges(("e", "f"), ("e", "a"))
+
+    dg.connect_edges(("a", "b"), ("a", "c"), ("a", "e"))
+    dg.connect_edges(("b", "d"), ("b", "e"), ("c", "e"), ("d", "c"))
 
     print(f"Adjacency-list: {dg.adj_list}")
+
+    print(f"Paths between 'a' & 'c' are: {dg.get_paths('a', 'c', [])}")
+    print(f"Shortest path between 'a' & 'c' are: {dg.get_shortest_path('a', 'c')}")
+    print(f"DFS walk of the graph: {dfs_traversal(dg, 'a', [], visited=set())}")
